27b11c0f3673b0a855e85e0ae7db77f573f6eece
matthieuphilippemarechal <mathieu1290@gmail.com>
2023-02-20 12:02:01 -0300

    create repertory

--- dashboard_uber_NYC.py
+++ dashboard_uber_NYC.py
@@ -18,7 +18,7 @@ days_list = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sund
 hours_list = ['0-3','3-6','6-9','9-12','12-15','15-18','18-21','21-24']
 
 day_choice = st.sidebar.selectbox("Choose the day in week",days_list)
-hour_choice = st.sidebar.selectbox("Choose the hour in the day",hours_list)
+hour_choice = st.sidebar.selectbox("Choose the hours in the day",hours_list)
 
 st.write('Greater value of "Hot spot" means greater demand.')
 
